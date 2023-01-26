67f768ecbbc4e5fa2370305f67375b9e12bf8d0c
Lite009 <97386083+Lite009@users.noreply.github.com>
2023-01-26 19:56:02 +0530

    vince: adapt to Arrow-OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_vince.mk
+    $(LOCAL_DIR)/arrow_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_vince-user \
-    havoc_vince-userdebug \
-    havoc_vince-eng
+    arrow_vince-user \
+    arrow_vince-userdebug \
+    arrow_vince-eng
